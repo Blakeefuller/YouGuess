4f2bd89d040ea8bae71c90f5da8b311c90eb5639
Blakeefuller <Blakeefuller@gmail.com>
2022-05-18 15:34:07 -0700

    updated queriesandsample.sql file

--- YouGuessDataDefinitionQueriesAndSample.sql
+++ YouGuessDataDefinitionQueriesAndSample.sql
@@ -37,7 +37,8 @@ CREATE TABLE `YouTube_Channels` (
 
 LOCK TABLES `YouTube_Channels` WRITE;
 /*!40000 ALTER TABLE `YouTube_Channels` DISABLE KEYS */;
-INSERT INTO `YouTube_Channels` VALUES (0, 'SyKe', 16, 'https://yt3.ggpht.com/ytc/AKedOLTvs0ScA2dRb2zjwR6fPdCLgAwKTVIiOlLLjfzBTg=s88-c-k-c0x00ffffff-no-rj'), (1, 'PewDiePie', 111000000, 'https://yt3.ggpht.com/5oUY3tashyxfqsjO5SGhjT4dus8FkN9CsAHwXWISFrdPYii1FudD4ICtLfuCw6-THJsJbgoY=s88-c-k-c0x00ffffff-no-rj');
+INSERT INTO `YouTube_Channels` (`channelID`, `name`, `numSubscribers`, `channelPhotoUrl`) VALUES (0, 'SyKe', 16, 'https://yt3.ggpht.com/ytc/AKedOLTvs0ScA2dRb2zjwR6fPdCLgAwKTVIiOlLLjfzBTg=s88-c-k-c0x00ffffff-no-rj');
+INSERT INTO `YouTube_Channels` (`channelID`, `name`, `numSubscribers`, `channelPhotoUrl`) VALUES (1, 'PewDiePie', 111000000, 'https://yt3.ggpht.com/5oUY3tashyxfqsjO5SGhjT4dus8FkN9CsAHwXWISFrdPYii1FudD4ICtLfuCw6-THJsJbgoY=s88-c-k-c0x00ffffff-no-rj');
 /*!40000 ALTER TABLE `YouTube_Channels` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -45,16 +46,16 @@ UNLOCK TABLES;
 -- Table structure for table `YouTube_Videos`
 --
 
-CREATE TABLE `YouTube_Videos` (
-    `title` VARCHAR(255) NOT NULL,
-    `thumbnailURL` VARCHAR(255) NOT NULL,
-    `numViews` INT NOT NULL,
-    `channelID` INT,
-    FOREIGN KEY('channelID') REFERENCES 'YouTube_Channels' ('channelID')
-) ENGINE=INNODB;
+CREATE TABLE `YouTube_Videos` ( 
+    `channelID` INT(11) NOT NULL,  
+    `title` VARCHAR(255) NOT NULL,  
+    `thumbnailURL` VARCHAR(255) NOT NULL,  
+    `numViews` INT(11) NOT NULL,    
+    PRIMARY KEY(`channelID`)
+) ENGINE = InnoDB;
 
-INSERT INTO YouTube_Videos ('title', 'thumbnailURL', 'numViews', 'channelID') VALUES ('An Advanced Warfare Montage By SyKe Buritoe','https://i.ytimg.com/vi/skXvYAmQbaY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB9n_KgrgGpksGJ0v7MWDiHOE6Byw', 118, 0);
-INSERT INTO YouTube_Videos ('title', 'thumbnailURL', 'numViews', 'channelID') VALUES ('Congratulations', 'https://i.ytimg.com/an_webp/PHgc8Q6qTjc/mqdefault_6s.webp?du=3000&sqp=COCZ8pMG&rs=AOn4CLAHovwlwKDw8bqHGQlFjRYiMplqRg', 222711307, 1);
+INSERT INTO YouTube_Videos (`title`, `thumbnailURL`, `numViews`, `channelID`) VALUES ('An Advanced Warfare Montage By SyKe Buritoe','https://i.ytimg.com/vi/skXvYAmQbaY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB9n_KgrgGpksGJ0v7MWDiHOE6Byw', 118, 0);
+INSERT INTO YouTube_Videos (`title`, `thumbnailURL`, `numViews`, `channelID`) VALUES ('Congratulations', 'https://i.ytimg.com/an_webp/PHgc8Q6qTjc/mqdefault_6s.webp?du=3000&sqp=COCZ8pMG&rs=AOn4CLAHovwlwKDw8bqHGQlFjRYiMplqRg', 222711307, 1);
 
 --
 -- Table structure for table `Users`
@@ -68,8 +69,8 @@ CREATE TABLE Users (
     PRIMARY KEY(`userID`) 
 ) ENGINE=INNODB;
 
-INSERT INTO `Users` (`email`, `password`, `username`) VALUES ('nguyejo5@oregonstate.edu', 'nezuko4ever', 'John');
-INSERT INTO `Users` (`email`, `password`, `username`) VALUES ('fullerbl@oregonstate.edu', 'StephenTheGOAT!0', 'Blake');
+INSERT INTO `Users` (`userID`, `email`, `password`, `username`) VALUES (0 ,'nguyejo5@oregonstate.edu', 'nezuko4ever', 'John');
+INSERT INTO `Users` (`userID`, `email`, `password`, `username`) VALUES (1 ,'fullerbl@oregonstate.edu', 'StephenTheGOAT!0', 'Blake');
 
 
 --
@@ -77,28 +78,30 @@ INSERT INTO `Users` (`email`, `password`, `username`) VALUES ('fullerbl@oregonst
 --
 
 CREATE TABLE High_Scores (
+    `userID` int(11) NOT NULL,
     `scoreID` INT AUTO_INCREMENT PRIMARY KEY,
-    `highScore` INT NOT NULL
+    `highScore` INT NOT NULL,
     `gameMode` VARCHAR(255) NOT NULL,
+    `date` date NOT NULL,
     FOREIGN KEY(`userID`) REFERENCES Users (`userID`)
 ) ENGINE=INNODB;
 
-INSERT INTO High_Scores ('highScore', 'gameMode', 'userID') VALUES (10, 'subscribers', 0);
-INSERT INTO High_Scores ('highScore',' gameMode', 'userID') VALUES (8, 'subscribers', 1);
+INSERT INTO High_Scores (`highScore`, `gameMode`, `userID`) VALUES (10, 'subscribers', 0);
+INSERT INTO High_Scores (`highScore`, `gameMode`, `userID`) VALUES (8, 'subscribers', 1);
 
 --
 -- Table structure for table `Friends_List`
 --
 
 CREATE TABLE Friends_List (
-    `userID` int(11) UNIQUE NOT NULL PRIMARY KEY,
+    `userID` int(11) NOT NULL PRIMARY KEY,
     `friendID` int(11) UNIQUE NOT NULL,
     FOREIGN KEY(`userID`) REFERENCES Users (`userID`),
     FOREIGN KEY(`friendID`) REFERENCES Users (`userID`)
 ) ENGINE=INNODB;
 
-INSERT INTO Friends_List ('friendID') VALUES (1) where `userID`=0; 
-INSERT INTO Friends_List ('friendID') VALUES (0) where `userID`=1; 
+INSERT INTO Friends_List (`userID`, `friendID`) VALUES (0, 1); 
+INSERT INTO Friends_List (`userID`, `friendID`) VALUES (1, 0); 
 
 --
 -- Table structure for table `Items_List`
@@ -106,14 +109,14 @@ INSERT INTO Friends_List ('friendID') VALUES (0) where `userID`=1;
 
 
 CREATE TABLE Items_List (
-    `userID` int(11) UNIQUE NOT NULL PRIMARY KEY,
+    `userID` int(11) NOT NULL PRIMARY KEY,
     `skipQuantity` int NOT NULL,
     `reviveQuantity` int NOT NULL,
     FOREIGN KEY(`userID`) REFERENCES Users (`userID`)
 ) ENGINE=INNODB;
 
-INSERT INTO Items_List ('skipQuantity', `reviveQuantity`) VALUES (99999, 99999) where `userID`=0; 
-INSERT INTO Items_List ('skipQuantity', `reviveQuantity`) VALUES (99999, 99999) where `userID`=1; 
+INSERT INTO Items_List (`userID`, `skipQuantity`, `reviveQuantity`) VALUES (0 ,99999, 99999);
+INSERT INTO Items_List (`userID`, `skipQuantity`, `reviveQuantity`) VALUES (1, 99999, 99999); 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
